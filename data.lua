218c1d76076cfa7a4bec4bd5a54eeec21fe10f86
ZwerOxotnik <zweroxotnik@gmail.com>
2019-11-05 13:19:06 +0300

    Update thumbnail

--- data.lua
+++ data.lua
@@ -2,7 +2,7 @@ data:extend{
     {
         type = "sprite",
         name = "lawful-button-sprite",
-        filename = "__lawful-evil__/graphics/gavel.png",
+        filename = "__lawful-evil__/thumbnail.png",
         width = 512,
         height = 512,
         flags = {
